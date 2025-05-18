e305bc37df73bd8d0ea75eebfb7b5777a4302543
Tim Lensen <timlensen@gmail.com>
2025-05-18 13:01:59 +0200

    test sample

--- powerup.js
+++ powerup.js
@@ -1,4 +1,4 @@
-TrelloPowerUp.initialize({
+/* TrelloPowerUp.initialize({
   'board-buttons': function(t) {
     return [{
       icon: 'https://cdn-icons-png.flaticon.com/512/1680/1680899.png',
@@ -10,6 +10,39 @@ TrelloPowerUp.initialize({
           height: 250
         });
       }
+    }];
+  }
+}); */
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
+window.TrelloPowerUp.initialize({
+  'board-buttons': function (t, opts) {
+    return [{
+      // we can either provide a button that has a callback function
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'Callback',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // or we can also have a button that is just a simple url
+      // clicking it will open a new tab at the provided url
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'URL',
+      condition: 'always',
+      url: 'https://trello.com/inspiration',
+      target: 'Inspiring Boards' // optional target for above url
     }];
   }
 });
